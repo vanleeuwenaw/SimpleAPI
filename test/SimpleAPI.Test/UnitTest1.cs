821f1f99dc746e7f86e39a7a76a6af7d1c826a0d
Anthony van Leeuwen <vanleeuwenaw@roadrunner.com>
2020-03-27 20:21:11 -0700

    Initial Commit

---
+++ test/SimpleAPI.Test/UnitTest1.cs
@@ -0,0 +1,28 @@
+using System;
+using Xunit;
+using System.Windows;
+using SimpleAPI.Controllers;
+
+namespace SimpleAPI.Test
+{
+    public class UnitTest1
+    {
+        ValuesController controller = new ValuesController();
+
+        [Fact]
+        public void GetReturnsCorrectNumber()
+        {
+            var returnValue = controller.Get(1);
+            
+            Assert.Equal("value1", returnValue.Value);
+            
+            
+        }
+
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
